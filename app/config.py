ed143980b6e9937ada684927c2d8d2e6d79f5bbc
Akashptl <akashkalathiya46@gmail.com>
2025-08-28 17:03:33 +0530

    chore: Update MongoDB connection string and add new BSON/JSON dump files

--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    MONGO_URL: str = "mongodb://localhost:27017"
+    # MONGO_URL: str = "mongodb://localhost:27017"
+    MONGO_URL: str = "MONGO_URL: str = "mongodb+srv://akashkalathiya46_db_user:rcJpdVQsxaUUzbCQ@aichatbot.7qy2mj9.mongodb.net/ai_chat_bot""
+
     MONGO_DB_NAME: str = "ai_chat_bot"
 
     class Config:
